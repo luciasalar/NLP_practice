c82c15a02857734eaf30ab12a0268e8d2af2da79
luciasalar <luciasalar@gmail.com>
2017-11-12 15:18:19 +0000

    new

--- clean_data.py
+++ clean_data.py
@@ -5,36 +5,38 @@ from nltk.stem import PorterStemmer
 from nltk import FreqDist
 import string
 import re
-import enchant
+#import enchant
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def preprocess(sent):
 
-	words = sent.split()
-	new_words = []
-	new_words2 =[]
-	# ps = PorterStemmer()
-	
-	for w in words:
-		w = w.lower().replace("**bobsnewline**","")
-		# remove non English word characters
-		w = re.sub(r'[^\x00-\x7F]+',' ', w)
-		# remove puncutation 
-		w = re.sub(r'[^\w\s]','',w)
-		# w = ps.stem(w)
-		new_words.append(w)
-		
-	return ' '.join(new_words)
+    words = str(sent).split()
+    new_words = []
+    new_words2 =[]
+    # ps = PorterStemmer()
+    
+    for w in words:
+        w = w.lower().replace("**bobsnewline**","").PorterStemmer()
+        # remove non English word characters
+        w = re.sub(r'[^\x00-\x7F]+',' ', w)
+        # remove puncutation 
+        w = re.sub(r'[^\w\s]','',w)
+        # w = ps.stem(w)
+        new_words.append(w)
+        
+    return ' '.join(new_words)
 
 
 # def filter_non_engilsh(sent):
-# 	english = []
-# 	d = enchant.Dict("en_US")
-		
-# 	if d.check(new_text) == True:
-#  		english.append(new_text)
+#   english = []
+#   d = enchant.Dict("en_US")
+        
+#   if d.check(new_text) == True:
+#       english.append(new_text)
 
 
-# 	return ' '.join(english)
+#   return ' '.join(english)
 swl_data = pd.read_csv('user_all_status_swl2.csv')
 
 # drop column 
@@ -42,66 +44,98 @@ swl_data = swl_data.drop(swl_data.columns[[0]],axis = 1)
 # print (swl_data)
 swl_data = swl_data.groupby(['user_id'])
 sample = swl_data['status']
+
+strings_format = []
+for i in sample:
+    strings_format.append((str(i[0]), str(i[1])))
+
 # print(type(sample))
 
-def word_count(raw_text):
-	for x in sample:
-		x = str(x)
-		new_text = preprocess(x)
-		fdist = FreqDist(new_text.split())
-		vocab = fdist.keys()
-		print(fdist["he"])
+swl_data_test['status'] = swl_data_test['status'].apply(lambda x: str(x).lower())
 
-#print(word_count(sample))
+swl_data_test['status'] = swl_data_test['status'].apply(preprocess)
+
+freqdsts = swl_data_test.groupby('user_id').apply((lambda x: FreqDist(str(x).split())))
+freqdsts[1]
+
+#regular expression ???
+#You are working on the whole string and not on a list of tokens (this is what .split() does. Just remove it)
+#Also do you want to work on the 'status' column or on everything?
+clean1 = swl_data_test['status'].apply((lambda x: re.sub(r'[^\w\s]','',str(x))))
+
+
+###word cloud
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+wordcloud = WordCloud().generate(str(swl_data_test['status']))
+
+
+
+
+#######
+
+# def word_count(raw_text):
+#     for x in sample:
+#         x = str(x)
+#         new_text = preprocess(x)
+#         fdist = FreqDist(new_text.split())
+#         vocab = fdist.keys()
+# #       swl_data_test['fdist'] = fdist
+#         print(len(fdist))
+
+
+# print(word_count(sample))
 
 
 
 # count modal verbs
 
 def modal_count(raw_text):
-	new_text =[]
-	for x in raw_text:
-		x = str(x)
-		new = preprocess(x)
-		new_text.append(new)
-		fdist = FreqDist(new_text.split())
-		modals = ['can']
-		for m in modals:
-			print (m + ':', fdist[m])
+    new_text =[]
+    for x in raw_text:
+        x = str(x)
+        new = preprocess(x)
+        new_text.append(new)
+        fdist = FreqDist(new_text.split())
+        modals = ['can']
+        for m in modals:
+            print (m + ':', fdist[m])
 
 #print(modal_count(sample))
 
 
 def freq_list(raw_text):
-	new_text =[]
-	for x in raw_text:
-		x = str(x)
-		new = preprocess(x)
-		new_text.append(new)
-	fdist_list = []
-	for i in new_text:
-		fdist = FreqDist(i.split())
-		fdist_list.append(fdist)
+    new_text =[]
+    for x in raw_text:
+        x = str(x)
+        new = preprocess(x)
+        new_text.append(new)
+    fdist_list = []
+    for i in new_text:
+        fdist = FreqDist(i.split())
+        fdist_list.append(fdist)
 
-	return fdist_list
+    return fdist_list
 
-			#return fdist_list
+            #return fdist_list
 
 print(freq_list(sample))
 
 # def bigram(raw_text):
-# 	new_text =[]
-# 	for x in sample:
-# 		x = str(x)
-# 		new = preprocess(x)
-# 		new_text.append(new)
-# 		bigram_text = []
-# 		for i in new_text:
-# 			bi = nltk.bigrams(i.split())
-# 			bigram_text.append(bi)
-# 		return bigram_text
+#   new_text =[]
+#   for x in sample:
+#       x = str(x)
+#       new = preprocess(x)
+#       new_text.append(new)
+#       bigram_text = []
+#       for i in new_text:
+#           bi = nltk.bigrams(i.split())
+#           bigram_text.append(bi)
+#       return bigram_text
 
-		
+        
 # print (bigram(sample))
 
 
@@ -126,9 +160,9 @@ print(freq_list(sample))
 
 
 # for x in sample:
-# 	x = str(x)
-# 	new_text = preprocess(x)
-# 	print(new_text)
+#   x = str(x)
+#   new_text = preprocess(x)
+#   print(new_text)
 
 
 
@@ -163,15 +197,15 @@ print(freq_list(sample))
 
 # def tokenize_words(x):
 
-# 	tokenizer = RegexpTokenizer(r'\w+')
-# 	token_status = x.apply(lambda row: tokenizer.tokenize(row['status']), axis=1)
-# 	return token_status 
+#   tokenizer = RegexpTokenizer(r'\w+')
+#   token_status = x.apply(lambda row: tokenizer.tokenize(row['status']), axis=1)
+#   return token_status 
 
 # print(tokenize_words(swl_data_s))
 # words = tokenize_words(swl_data_s)
 
 # def word_count():
-# 	token_status = x.apply(lambda row: len(row['status']), axis=1)
+#   token_status = x.apply(lambda row: len(row['status']), axis=1)
 
 
 
